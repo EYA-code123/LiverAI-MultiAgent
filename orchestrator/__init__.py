59e42769d6503dea94749ace845c0642f8078a03
EYA-code123 <ayaboukhili07@gmail.com>
2026-08-19 20:19:08 +0100

    Update __init__.py

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -1,8 +1,3 @@
-%%writefile /content/LiverAI-MultiAgent/orchestrator/__init__.py
+from .liver_orchestrator import LiverAIOrchestrator
 
-from .liver_orchestrator import LiverAIOrchestrator, print_results
-
-__all__ = [
-    "LiverAIOrchestrator",
-    "print_results"
-]
+__all__ = ["LiverAIOrchestrator"]
