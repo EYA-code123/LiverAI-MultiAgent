4646c5e1a9d07b206961ae652a8d7d0c89a07b7f
EYA-code123 <ayaboukhili07@gmail.com>
2026-08-29 09:55:57 +0100

    Create __init__.py

---
+++ coordinator/__init__.py
@@ -0,0 +1,13 @@
+from .coordinator import LiverCoordinator
+from .trust import TrustManager
+from .conflict import ConflictDetector
+from .fusion import FusionEngine
+from .decision import DecisionEngine
+
+__all__ = [
+    "LiverCoordinator",
+    "TrustManager",
+    "ConflictDetector",
+    "FusionEngine",
+    "DecisionEngine"
+]
