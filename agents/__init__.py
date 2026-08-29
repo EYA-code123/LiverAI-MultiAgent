17e15366201cacf9b2a95455ae05f2a5db0c1766
EYA-code123 <ayaboukhili07@gmail.com>
2026-08-29 10:17:28 +0100

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -7,7 +7,7 @@ from .cirrhosis_agent import CirrhosisAgent
 from .fatty_liver_agent import FattyLiverAgent
 from .clinical_reasoning_agent import ClinicalReasoningAgent
 from .fibrosis_agent import FibrosisAgent
-from .tumor_agent import TumorClassificationAgent
+from .tumor_classification_agent import TumorClassificationAgent
 from .segmentation_agent import LiverSegmentationAgent
 
 __all__ = [
